0f99b473af312a8d57db2c7c821eb87d3c1da1e9
gabrielgag89@gmail.com <gabrielgag89@gmail.com@b6ca5b3c-0a19-57bd-4d67-60c3dafa623b>
2013-09-18 21:47:11 +0000

    Agregamos descripción del código de error.

---
+++ ReadWriteRS232/ReadWriteRS232/Variable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReadWriteRS232
+{
+    public class Variable
+    {
+        public String nombre;
+        public String valor;
+    }
+}
