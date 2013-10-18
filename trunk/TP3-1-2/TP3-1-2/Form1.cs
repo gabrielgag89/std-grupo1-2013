b42742bc7a0c43a0adc1b8b0a4c62a5b1ce4a2ca
gabrielgag89@gmail.com <gabrielgag89@gmail.com@b6ca5b3c-0a19-57bd-4d67-60c3dafa623b>
2013-10-18 00:29:52 +0000

    Agrego TP 3 - Parte 1.2

---
+++ trunk/TP3-1-2/TP3-1-2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TP3_1_2
+{
+	public partial class Form1 : Form
+	{
+		#region Events
+
+		public Form1()
+		{
+			InitializeComponent();
+			BuildTrama();
+		}
+
+		private void KeyUp(object sender, KeyEventArgs e)
+		{
+			try
+			{
+				BuildTrama();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void ValueChanged(object sender, EventArgs e)
+		{
+			try
+			{
+				BuildTrama();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		#endregion
+
+		#region Methods
+
+		public void BuildTrama()
+		{
+			try
+			{
+				string idMachine = Convert.ToString(int.Parse(nudIdMachine.Value.ToString()), 2).PadLeft(9, '0');
+				string functCode = Convert.ToString(int.Parse(nudFunctCode.Value.ToString()), 2).PadLeft(5, '0');
+				string startAddress = Convert.ToString(int.Parse(nudStartAddress.Value.ToString()), 2).PadLeft(16, '0');
+				string varQuantity = Convert.ToString(int.Parse(nudVarQuantity.Value.ToString()), 2).PadLeft(9, '0');
+
+				txtIdMachine.Text = idMachine;
+				txtFunctCode.Text = functCode;
+				txtStartAddress.Text = startAddress;
+				txtVarQuantity.Text = varQuantity;
+
+				txtTrama.Text = string.Format("[{0} {1} {2} 0 {3}]",
+												idMachine,
+												functCode,
+												varQuantity,
+												startAddress);
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		#endregion
+	}
+}
