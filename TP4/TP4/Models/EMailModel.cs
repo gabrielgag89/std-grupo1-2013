e186d6805ccb1f148fde93a412f143fd93229506
gabrielgag89@gmail.com <gabrielgag89@gmail.com@b6ca5b3c-0a19-57bd-4d67-60c3dafa623b>
2013-11-01 21:36:18 +0000

    Agrego el TP 4 y subo el TP 3-2 arreglado.

---
+++ TP4/TP4/Models/EMailModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP4.Models
+{
+	public class EMailModel
+	{
+		public int Numero { get; set; }
+		public string Remitente { get; set; }
+		public string FechaHora { get; set; }
+		public string Corriente { get; set; }
+		public string Potencia { get; set; }
+		public string Presion { get; set; }
+		public string Temperatura { get; set; }
+		public string Tension { get; set; }
+	}
+}
